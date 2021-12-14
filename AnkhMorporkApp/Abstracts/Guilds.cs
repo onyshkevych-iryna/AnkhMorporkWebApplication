1ea73cdcd5438e5147cf59e51f1a9f6770ba1302
onyshkevych-iryna <iryna.onyshkevych@valtech.com>
2021-12-14 12:31:29 +0200

    AnkhMorporkApp removed

--- AnkhMorporkApp/Abstracts/Guilds.cs
+++
@@ -1,9 +0,0 @@
-﻿using AnkhMorporkApp.Models;
-
-namespace AnkhMorporkApp.Abstracts
-{
-    public abstract class Guilds <T>
-    {
-        public abstract void InteractionWithPlayer(Player player, T guildMember);
-    }
-}
